e1a3dc729925ae5f7016c6c8f7852ca8503bce66
Jimmy <86983021+Lyoko2696@users.noreply.github.com>
2021-11-12 12:17:12 -0500

    Ajout gestion erreur update, optimisation gestion erreurs et enlever axios.

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,6 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../manageDb');
-var axios = require('axios').default;
 
 /* Variable globales */
 var BDD;
@@ -30,13 +29,15 @@ router.get('/contact', function(req, res, next) {
 router.get('/logout', function(req, res, next) {
   username = "";
   password = "";
-  console.log("Déconnection réussie.");
+  console.log("Log /update: Déconnecté!");
   res.redirect("/");
 });
 
 /* POST Page d'accueil de l'utilisateur. */
 router.post('/accueil', function(req, res, next) {
   // Met à jour BDD.
+  var eTarget = req.body.eTarget;
+  var eMessage = req.body.eMessage;
   readDb().then(
     async function(myDb) {
       // Store le résultat de readDb dans la variable globale.
@@ -45,48 +46,51 @@ router.post('/accueil', function(req, res, next) {
         username = req.body.username;
         password = req.body.password;
       }
-      console.log("Tentative de connection: "+username+" MdP: "+password+"...");
+      if (eTarget == "erreur") {
+        throw("5 Erreur: Target mal/non définie. Ni message, ni password");
+      }
+      console.log("Log /accueil: Connection "+username);
       var userCheck = await checkUser(BDD, username);
       return userCheck;
     },
     function(error) {
-      return Promise.reject(error);
-  })
-  .then(
+      throw(error);
+    }
+  ).then(
     async function(userId) {
       var pwdCheck = await checkUserPassword(BDD, userId-1, password);
       return pwdCheck;
     },
     function(error) {
-      return Promise.reject(error);
-  })
-  .then(
+      throw(error);
+    }
+  ).then(
     function(userId) {
-      console.log("Connection réussi!");
-      res.render('./pages/accueil', { title: 'Accueil', myDb: BDD, user: userId });
+      console.log("Log /accueil: Connecté!");
+      res.render('./pages/accueil', { title: 'Accueil', myDb: BDD, user: userId, erreurMessage: eMessage, erreurTarget: eTarget});
     },
     function(error) {
-      console.log("Erreur /accueil :");
-      console.log(error);
-      if ( (error[0] == '1') || (error[0] == '2')) {
-        /* Remise à zéro de la session et redirection vers "/" avec un message d'erreur. */
-        username = "";
-        password = "";
-        res.render('./pages/index', { title: 'Express', messageErreur: "Erreur: Nom d'utilisateur ou mot de passe incorrect!" });
-      } else if (error[0] == '3') {
-        /* Redirige sur accueil pour revérifier si la Banque de donnée est définie. */
-        res.render('./pages/index', { title: 'Express', messageErreur: "Erreur: Probléme de lecture de la banque de donnée!" });
-      } else {
-        /* Remise à zéro de la session et redirection vers "/" avec un message d'erreur. */
-        username = "";
-        password = "";
-        res.render('./pages/index', { title: 'Express', messageErreur: "Erreur: Erreur inattendu!" });
-      }
-  })
-  .catch(function (err) {
-    console.log("Catch /accueil : ");
-    console.log(err);
-  });
+      throw(error);
+    }
+  ).catch(function (error) {
+    console.log("Catch /accueil:");
+    console.log(error);
+    var message;
+    /* Remise à zéro de la session et définition de message d'erreur à afficher sur la page selon le numéro d'erreur. */
+    if ( (error[0] == '1') || (error[0] == '2')) {
+      message = "Erreur: Nom d'utilisateur ou mot de passe incorrect!";
+    } else if (error[0] == '3') {
+      message = "Erreur: Probléme de lecture de la banque de donnée! Connection fermée.";
+    } else if (error[0] == '5') {
+      message = "Erreur inattendu dans /update. Connection fermée.";
+    } else {
+      message = "Erreur inattendu dans /accueil. Connection fermée.";
+    }
+    username = "";
+    password = "";
+    res.render('./pages/index', { title: 'Express', messageErreur: message });
+    }
+  );
 });
 
 /* POST Mise à jour de la banque de donnée demander par l'utilisateur. */
@@ -96,97 +100,93 @@ router.post('/update', function(req, res, next) {
   var userId = req.body.userId;
   /* Requète UPDATE et redirection vers /accueil en POST (307). */
   modifyDb(data, target, userId).then(
-    function(value) {
+    function() {
       if( target == "password") {
         /* Mise à jour de mot de passe dans le serveur pour permettre la reconnection à la page /accueil. */
         password = data;
-        axios.post('http://localhost:3000/accueil', {
-          username: username,
-          password: password
-        })
-        .then(function (response) {
-          console.log("Axios response : "+ response.statusText);
-          res.redirect(307, "/accueil");
-        })
-        .catch(function (error) {
-          console.log("Axios erreur : ");
-          console.log(JSON.stringify(error));
-          res.send(error);
-        });
-      } else {
-        res.redirect(307, "/accueil");
       }
+      res.redirect(307, "/accueil");
     },
     function(error) {
-      console.log(error);
-
-      /* Remise à zéro de la session et redirection vers / avec un message d'erreur. */
-      username = "";
-      password = "";
-      res.render('./pages/index', { title: 'Express', messageErreur: "Erreur: Un problème lors de la modification est survenu. Connection fermée." });
+      throw(error);
     }
-  ).catch(function (err) {
-    console.log("/update : ");
-    console.log(err);
+  ).catch(function (error) {
+    console.log("Catch /update:");
+    console.log(error);
+
+    var message;
+    if(target == "message") {
+      message = "Erreur: Un problème est survenu lors de la modification du texte d'accueil.";
+    } else if (target == "password") {
+      message = "Erreur: Un problème est survenu lors de la modification du mot de passe.";
+    } else {
+      target = "erreur";
+    }
+    res.render('./pages/accueil', { title: 'Accueil', myDb: BDD, user: userId, erreurMessage: message, erreurTarget: target});
   });
 });
 
 /* Fontion qui compare le nom d'utilisateur donnée à ceux contenue dans la banques de donnée */
+/* et retourne un objet promesse avec le id de l'utilisateur s'il existe ou un message d'erreur si l'utilisateur n'existe pas. */
 function checkUser(db, user) {
   for (var i = 0;i <= (db.length-1); i++) {
       if ( (db[i].login) == user) {
         return Promise.resolve(db[i].id);
       }
   }
-  return Promise.reject("1 Erreur: Utilisateur '"+user+"' n'existe pas");
+  return Promise.reject("1 checkUser Erreur: Utilisateur '"+user+"' n'existe pas");
 }
 
-/* Fontion qui compare le mot de passe donnée à celui associé à l'utilisateur spécifié. */
+/* Fontion qui compare le mot de passe donnée à celui associé à l'utilisateur spécifié */
+/* et retourne un objet promesse avec le id de l'utilisateur confirmé ou un message d'erreur si le mot de passe n'est pas bon. */
 function checkUserPassword(db, id, password) {
   if ( (db[id].password) == password) {
     return Promise.resolve(db[id].id);
   } else {
-    return Promise.reject("2 Erreur : Mot de passe incorrecte!");
+    return Promise.reject("2 checkUserPassword Erreur: Mot de passe incorrecte!");
   }
 }
 
-/* Fonction qui met à jour la banque de donnée */
+/* Fonction qui intéroge toute la banque de donnée et retourne un objet promesse avec les données ou un message d'erreur une fois celle-çi terminé. */
 async function readDb() {
   var querystring = 'SELECT * FROM login';
+  /* Création de la promesse qui confirmera si le requète SQL est teminé ou s'il y a eu un erreur. */
     var readPromise = await new Promise((resolve, reject) => {
-      var query = connection.query(querystring, function(err, rows, fields) {
+      connection.query(querystring, function(err, rows, fields) {
         if (!err) {
-          console.log("Ma requête SELECT est passée !");
+          console.log("Log readDb(): Requête SELECT OK!");
           resolve(JSON.parse(JSON.stringify(rows)));
         } else {
-          reject("3 Erreur readDb : "+err);
+          reject("3 readDb() "+err);
         };
       });
     });
   return readPromise;
 }
 
-/* Fonction qui fait la requète SQL qui met à jour la donnée spécifié pour l'utilisateur spécifier (UPDATE) */
+/* Fonction qui fait la requète SQL qui met à jour (UPDATE) la donnée spécifié pour l'utilisateur spécifier */
+/* et retourne un objet promesse avec le résultat ou un message d'erreur une fois celle-çi temriné.  */
 async function modifyDb(data, target, userId) {
+  /* Construction de la requète selon le type de donnée à modifier. */
   var querystring = 'UPDATE login SET ';
+  /* Changement de target pour la construction du console.log dans updateDb(). */
   if (target == "message") {
     querystring += "texteAccueil = '"+data+"' ";
-    /* Changement de target pour la construction du console.log dans updateDb(). */
     target = "Texte d'accueil : ";
   } else if (target == "password"){
     querystring += "password = '"+data+"' ";
-    /* Changement de target pour la construction du console.log dans updateDb(). */
     target = "MdP : ";
   }
   querystring += "WHERE id = "+userId+";";
 
+  /* Création de la promesse qui confirmera si le requète SQL est teminé ou s'il y a eu un erreur. */
   var updatePromise = await new Promise((resolve, reject) => {
-    var query = connection.query(querystring, async function(err, rows, fields) {
+    connection.query(querystring, async function(err, rows, fields) {
       if (!err) {
-        console.log("Requète UPDATE: "+username+" => "+target+data+" est passé!");
-        resolve(JSON.parse(JSON.stringify(rows)));
+        console.log("Log modifyDb(): Requète UPDATE "+username+" => "+target+data+" OK!");
+        resolve();
       } else {
-        reject("4 Erreur update : "+JSON.stringify(err));
+        reject("4 modifyDb() "+err);
       };
     });
   }); 
